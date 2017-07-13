dab49810749955d6b8971c99f55fec9ddedb8df0
Lars Wirzenius <liw@liw.fi>
2017-07-13 18:59:56 +0300

    Add: structured logging from old qvarn

---
+++ slog/slog.py
@@ -0,0 +1,263 @@
+# slog.py - structured logging
+#
+# Copyright 2016-2017  QvarnLabs Ab
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import datetime
+import json
+import logging
+import os
+import _thread
+import time
+import traceback
+import syslog
+
+import slog
+
+
+class StructuredLog(object):
+
+    '''A structured logging system.
+
+    A structured log is one that can be easily parsed
+    programmatically. Traditional logs are free form text, usually
+    with a weakly enforced line structure and some minimal metadata
+    prepended to each file. This class produces JSON records instead.
+
+    See the separate manual for more background and examples of how to
+    use this system.
+
+    '''
+
+    def __init__(self):
+        self._msg_counter = slog.Counter()
+        self._context = {}
+        self._writers = []
+
+    def close(self):
+        for writer, _ in self._writers:
+            writer.close()
+        self._writers = []
+
+    def add_log_writer(self, writer, filter_rule):
+        self._writers.append((writer, filter_rule))
+
+    def set_context(self, new_context):
+        thread_id = self._get_thread_id()
+        self._context[thread_id] = new_context
+
+    def reset_context(self):
+        thread_id = self._get_thread_id()
+        self._context[thread_id] = None
+
+    def log(self, msg_type, **kwargs):
+        exc_info = kwargs.pop('exc_info', False)
+
+        log_obj = {
+            'msg_type': msg_type,
+        }
+        for key, value in kwargs.items():
+            log_obj[key] = self._convert_value(value)
+
+        self._add_extra_fields(log_obj, exc_info)
+        for writer, filter_rule in self._writers:
+            if filter_rule.allow(log_obj):
+                writer.write(log_obj)
+
+    def _convert_value(self, value):
+        # Convert a value into an form that's safe to write. Meaning,
+        # it can't be binary data, and it is UTF-8 compatible, if it's
+        # a string of any sort.
+        #
+        # Note that we do not need to be able to parse the value back
+        # again, we just need to write it to a log file in a form that
+        # the user will understand. At least for now.
+        #
+        # We can't do this while encoding JSON, because the Python
+        # JSON library doesn't seem to allow us to override how
+        # encoding happens for types it already knows about, only for
+        # other types of values.
+
+        converters = {
+            int: self._nop_conversion,
+            float: self._nop_conversion,
+            bool: self._nop_conversion,
+            str: self._nop_conversion,
+            type(None): self._nop_conversion,
+
+            list: self._convert_list_value,
+            dict: self._convert_dict_value,
+            tuple: self._convert_tuple_value,
+        }
+
+        value_type = type(value)
+        assert value_type in converters, \
+            'Unknown data type {}'.format(value_type)
+        func = converters[type(value)]
+        converted = func(value)
+        return converted
+
+    def _nop_conversion(self, value):
+        return value
+
+    def _convert_list_value(self, value):
+        return [self._convert_value(item) for item in value]
+
+    def _convert_tuple_value(self, value):
+        return tuple(self._convert_value(item) for item in value)
+
+    def _convert_dict_value(self, value):
+        return {
+            self._convert_value(key): self._convert_value(value[key])
+            for key in value
+        }
+
+    def _add_extra_fields(self, log_obj, stack_info):
+        log_obj['_msg_number'] = self._get_next_message_number()
+        log_obj['_timestamp'] = self._get_current_timestamp()
+        log_obj['_process_id'] = self._get_process_id()
+        log_obj['_thread_id'] = self._get_thread_id()
+        log_obj['_context'] = self._context.get(self._get_thread_id())
+        if stack_info:
+            log_obj['_traceback'] = self._get_traceback()
+
+    def _get_next_message_number(self):
+        return self._msg_counter.increment()
+
+    def _get_current_timestamp(self):
+        return datetime.datetime.utcnow().isoformat(' ')
+
+    def _get_process_id(self):
+        return os.getpid()
+
+    def _get_thread_id(self):
+        return _thread.get_ident()
+
+    def _get_traceback(self):
+        return traceback.format_exc()
+
+
+class SlogWriter(object):  # pragma: no cover
+
+    def write(self, log_obj):
+        raise NotImplementedError()
+
+    def close(self):
+        raise NotImplementedError()
+
+
+class NullSlogWriter(SlogWriter):  # pragma: no cover
+
+    def write(self, log_obj):
+        pass
+
+    def close(self):
+        pass
+
+
+class FileSlogWriter(SlogWriter):
+
+    def __init__(self):
+        self._log_filename = None
+        self._log_file = None
+        self._bytes_max = None
+        self._encoder = json.JSONEncoder(sort_keys=True)
+
+    def set_max_file_size(self, bytes_max):
+        self._bytes_max = bytes_max
+
+    def get_filename(self):
+        return self._log_filename
+
+    def get_rotated_filename(self, now=None):
+        prefix, suffix = os.path.splitext(self._log_filename)
+        if now is None:  # pragma: no cover
+            now = time.localtime()
+        else:
+            now = tuple(list(now) + [0]*9)[:9]
+        timestamp = time.strftime('%Y%m%dT%H%M%S', now)
+        return '{}-{}{}'.format(prefix, timestamp, suffix)
+
+    def set_filename(self, filename):
+        self._log_filename = filename
+        self._log_file = open(filename, 'a')
+
+    def write(self, log_obj):
+        if self._log_file:
+            self._write_message(log_obj)
+            if self._bytes_max is not None:
+                self._rotate()
+
+    def _write_message(self, log_obj):
+        msg = self._encoder.encode(log_obj)
+        self._log_file.write(msg + '\n')
+        self._log_file.flush()
+
+    def _rotate(self):
+        pos = self._log_file.tell()
+        if pos >= self._bytes_max:
+            self._log_file.close()
+            rotated = self.get_rotated_filename()
+            os.rename(self._log_filename, rotated)
+            self.set_filename(self._log_filename)
+
+    def close(self):
+        self._log_file.close()
+        self._log_file = None
+
+
+class SyslogSlogWriter(SlogWriter):  # pragma: no cover
+
+    def write(self, log_obj):
+        encoder = json.JSONEncoder(sort_keys=True)
+        s = encoder.encode(log_obj)
+        syslog.syslog(s)
+
+    def close(self):
+        pass
+
+
+class SlogHandler(logging.Handler):  # pragma: no cover
+
+    '''A handler for the logging library to capture into a slog.
+
+    In order to capture all logging.* log messages into a structured
+    log, configure the logging library to use this handler.
+
+    '''
+
+    def __init__(self, slog):
+        super(SlogHandler, self).__init__()
+        self.slog = slog
+
+    def emit(self, record):
+        log_args = dict()
+        for attr in dir(record):
+            if not attr.startswith('_'):
+                value = getattr(record, attr)
+                if not isinstance(value, (str, unicode, int, bool, float)):
+                    value = repr(value)
+                log_args[attr] = value
+        self.slog.log('logging', **log_args)
+
+
+def hijack_logging(slog):  # pragma: no cover
+    '''Hijack log messages that come via logging.* into a slog.'''
+
+    handler = SlogHandler(slog)
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
---
+++ slog/slog_tests.py
@@ -0,0 +1,258 @@
+# slog_tests.py - unit tests for structured logging
+#
+# Copyright 2016  QvarnLabs Ab
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import glob
+import json
+import os
+import shutil
+import tempfile
+import time
+import unittest
+
+import slog
+
+
+class StructuredLogTests(unittest.TestCase):
+
+    def setUp(self):
+        self.tempdir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.tempdir)
+
+    def create_structured_log(self):
+        filename = os.path.join(self.tempdir, 'slog.log')
+        writer = slog.FileSlogWriter()
+        writer.set_filename(filename)
+
+        log = slog.StructuredLog()
+        log.add_log_writer(writer, slog.FilterAllow())
+        return log, writer, filename
+
+    def read_log_entries(self, writer):
+        filename = writer.get_filename()
+        with open(filename) as f:
+            return [json.loads(line) for line in f]
+
+    def test_logs_in_json(self):
+        slog, writer, _ = self.create_structured_log()
+        slog.log('testmsg', foo='foo', bar='bar', number=12765)
+        slog.close()
+
+        objs = self.read_log_entries(writer)
+        self.assertEqual(len(objs), 1)
+        obj = objs[0]
+        self.assertEqual(obj['msg_type'], 'testmsg')
+        self.assertEqual(obj['foo'], 'foo')
+        self.assertEqual(obj['bar'], 'bar')
+        self.assertEqual(obj['number'], 12765)
+
+    def test_logs_two_lines_in_json(self):
+        slog, writer, _ = self.create_structured_log()
+        slog.log('testmsg1', foo='foo')
+        slog.log('testmsg2', bar='bar')
+        slog.close()
+
+        objs = self.read_log_entries(writer)
+        self.assertEqual(len(objs), 2)
+        obj1, obj2 = objs
+
+        self.assertEqual(obj1['msg_type'], 'testmsg1')
+        self.assertEqual(obj1['foo'], 'foo')
+
+        self.assertEqual(obj2['msg_type'], 'testmsg2')
+        self.assertEqual(obj2['bar'], 'bar')
+
+    def test_adds_some_extra_fields(self):
+        slog, writer, _ = self.create_structured_log()
+        slog.log('testmsg')
+        slog.close()
+
+        objs = self.read_log_entries(writer)
+        self.assertEqual(len(objs), 1)
+        obj = objs[0]
+        self.assertEqual(obj['msg_type'], 'testmsg')
+        self.assertIn('_timestamp', obj)
+        self.assertIn('_process_id', obj)
+        self.assertIn('_thread_id', obj)
+        self.assertEqual(obj['_context'], None)
+
+    def test_adds_context_when_given(self):
+        slog, writer, _ = self.create_structured_log()
+        slog.set_context('request 123')
+        slog.log('testmsg')
+        slog.close()
+
+        objs = self.read_log_entries(writer)
+        self.assertEqual(len(objs), 1)
+        obj = objs[0]
+        self.assertEqual(obj['_context'], 'request 123')
+
+    def test_resets_context_when_requested(self):
+        slog, writer, _ = self.create_structured_log()
+        slog.set_context('request 123')
+        slog.reset_context()
+        slog.log('testmsg')
+        slog.close()
+
+        objs = self.read_log_entries(writer)
+        self.assertEqual(len(objs), 1)
+        obj = objs[0]
+        self.assertEqual(obj['_context'], None)
+
+    def test_counts_messages(self):
+        slog, writer, _ = self.create_structured_log()
+        slog.log('testmsg')
+        slog.log('testmsg')
+        slog.close()
+
+        objs = self.read_log_entries(writer)
+        self.assertEqual(len(objs), 2)
+        self.assertEqual(objs[0]['_msg_number'], 1)
+        self.assertEqual(objs[1]['_msg_number'], 2)
+
+    def test_logs_traceback(self):
+        slog, writer, _ = self.create_structured_log()
+        slog.log('testmsg', exc_info=True)
+        slog.close()
+
+        objs = self.read_log_entries(writer)
+        self.assertEqual(len(objs), 1)
+        self.assertIn('_traceback', objs[0])
+
+    def test_logs_unicode(self):
+        log, writer, _ = self.create_structured_log()
+        log.log('testmsg', text='foo')
+        log.close()
+
+        objs = self.read_log_entries(writer)
+        self.assertEqual(len(objs), 1)
+        self.assertEqual(objs[0]['text'], 'foo')
+
+    def test_logs_nonutf8(self):
+        slog, writer, _ = self.create_structured_log()
+        notutf8 = '\x86'
+        slog.log('blobmsg', notutf8=notutf8)
+        slog.close()
+
+        objs = self.read_log_entries(writer)
+        self.assertEqual(len(objs), 1)
+        self.assertEqual(objs[0]['notutf8'], notutf8)
+
+    def test_logs_list(self):
+        slog, writer, _ = self.create_structured_log()
+        slog.log('testmsg', items=[1, 2, 3])
+        slog.close()
+
+        objs = self.read_log_entries(writer)
+        self.assertEqual(len(objs), 1)
+        self.assertEqual(objs[0]['items'], [1, 2, 3])
+
+    def test_logs_tuple(self):
+        slog, writer, _ = self.create_structured_log()
+        slog.log('testmsg', t=(1, 2, 3))
+        slog.close()
+
+        objs = self.read_log_entries(writer)
+        self.assertEqual(len(objs), 1)
+        # Tuples get returned as s list.
+        self.assertEqual(objs[0]['t'], [1, 2, 3])
+
+    def test_logs_dict(self):
+        slog, writer, _ = self.create_structured_log()
+        dikt = {
+            'foo': 'bar',
+            'yo': [1, 2, 3],
+        }
+        slog.log('testmsg', dikt=dikt)
+        slog.close()
+
+        objs = self.read_log_entries(writer)
+        self.assertEqual(len(objs), 1)
+        self.assertEqual(objs[0]['dikt'], dikt)
+
+    def test_logs_to_two_files(self):
+        filename1 = os.path.join(self.tempdir, 'slog1XS')
+        writer1 = slog.FileSlogWriter()
+        writer1.set_filename(filename1)
+
+        filename2 = os.path.join(self.tempdir, 'slog2')
+        writer2 = slog.FileSlogWriter()
+        writer2.set_filename(filename2)
+
+        log = slog.StructuredLog()
+        log.add_log_writer(writer1, slog.FilterAllow())
+        log.add_log_writer(writer2, slog.FilterAllow())
+
+        log.log('test', msg_text='hello')
+        objs1 = self.read_log_entries(writer1)
+        objs2 = self.read_log_entries(writer2)
+
+        self.assertEqual(objs1, objs2)
+
+
+class FileSlogWriterTests(unittest.TestCase):
+
+    def setUp(self):
+        self.tempdir = tempfile.mkdtemp()
+        self.filename = os.path.join(self.tempdir, 'foo.log')
+
+    def tearDown(self):
+        shutil.rmtree(self.tempdir)
+
+    def test_gets_initial_filename_right(self):
+        fw = slog.FileSlogWriter()
+        fw.set_filename(self.filename)
+        self.assertEqual(fw.get_filename(), self.filename)
+
+    def test_gets_rotated_filename_right(self):
+        fw = slog.FileSlogWriter()
+        fw.set_filename(self.filename)
+
+        now = time.struct_time((1969, 9, 1, 14, 30, 42, 0, 243, 0))
+        self.assertEqual(
+            fw.get_rotated_filename(now=now),
+            os.path.join(self.tempdir, 'foo-19690901T143042.log')
+        )
+
+    def test_creates_file(self):
+        fw = slog.FileSlogWriter()
+        filename = os.path.join(self.tempdir, 'slog.log')
+        fw.set_filename(filename)
+        self.assertTrue(os.path.exists(filename))
+
+    def test_rotates_after_size_limit(self):
+        fw = slog.FileSlogWriter()
+        filename = os.path.join(self.tempdir, 'slog.log')
+        fw.set_filename(filename)
+        fw.set_max_file_size(1)
+        fw.write({'foo': 'bar'})
+        filenames = glob.glob(self.tempdir + '/*.log')
+        self.assertEqual(len(filenames), 2)
+        self.assertTrue(filename in filenames)
+
+        rotated_filename = [x for x in filenames if x != filename][0]
+        objs1 = self.load_log_objs(filename)
+        objs2 = self.load_log_objs(rotated_filename)
+
+        self.assertEqual(len(objs1), 0)
+        self.assertEqual(len(objs2), 1)
+
+    def load_log_objs(self, filename):
+        with open(filename) as f:
+            return [json.loads(line) for line in f]
